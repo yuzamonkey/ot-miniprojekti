bd58be2e936944c183589ddc2352cd73189084fd
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2021-12-01 11:56:51 +0200

    add bookmark manager

---
+++ src/main/java/ot/miniprojekti/logic/BookmarkManager.java
@@ -0,0 +1,22 @@
+package ot.miniprojekti.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+import ot.miniprojekti.domain.Bookmark;
+
+public class BookmarkManager {
+
+    private List<Bookmark> bookmarks;
+
+    public BookmarkManager() {
+        this.bookmarks = new ArrayList<>();
+    }
+    
+    public void addBookmark(String name) {
+        this.bookmarks.add(new Bookmark(name));
+    }
+    
+    public List<Bookmark> getBookmarks() {
+        return this.bookmarks;
+    }
+}
