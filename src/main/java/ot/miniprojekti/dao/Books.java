2c9057db69a9c1180e348b52e7116d1ff8e19247
Jusa Mankki <jusamankki@gmail.com>
2021-12-06 13:16:52 +0200

    update checkstyle and fix checkstyle errors

--- src/main/java/ot/miniprojekti/dao/Books.java
+++ src/main/java/ot/miniprojekti/dao/Books.java
@@ -1,24 +1,22 @@
 package ot.miniprojekti.dao;
 
 import java.sql.*;
-import java.util.ArrayList;
-import ot.miniprojekti.domain.Bookmark;
 
 public class Books {
-    
+
     private Connection db;
-    
+
     public Books(String data) throws SQLException {
         db = DriverManager.getConnection("jdbc:sqlite:" + data);
         try {
-            PreparedStatement stmt = db.prepareStatement("CREATE TABLE IF NOT EXISTS books \n" + 
-                    "(id INTEGER PRIMARY KEY, author TEXT, title TEXT, isbn TEXT)");
+            PreparedStatement stmt = db.prepareStatement("CREATE TABLE IF NOT EXISTS books \n"
+                    + "(id INTEGER PRIMARY KEY, author TEXT, title TEXT, isbn TEXT)");
             stmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
-    
+
     public void add(String author, String title, String isbn) throws SQLException {
         PreparedStatement stmt = db.prepareStatement("INSERT INTO books (author, title, isbn) VALUES (?, ?, ?)");
         stmt.setString(1, author);
@@ -27,6 +25,5 @@ public class Books {
         stmt.executeUpdate();
         stmt.close();
     }
-    
-    
+
 }
