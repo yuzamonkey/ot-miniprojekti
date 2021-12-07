f4bcaf227c323cf8976c14e8f33849d1e7a8df02
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2021-12-07 20:18:42 +0200

    add ability to add/print videos and blogs

--- src/main/java/ot/miniprojekti/logic/BookmarkManager.java
+++ src/main/java/ot/miniprojekti/logic/BookmarkManager.java
@@ -2,18 +2,26 @@ package ot.miniprojekti.logic;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import ot.miniprojekti.domain.Podcast;
-import ot.miniprojekti.domain.Book;
+import ot.miniprojekti.dao.Blogs;
 import ot.miniprojekti.dao.Books;
 import ot.miniprojekti.dao.Podcasts;
+import ot.miniprojekti.dao.Videos;
+import ot.miniprojekti.domain.Blog;
+import ot.miniprojekti.domain.Book;
+import ot.miniprojekti.domain.Podcast;
+import ot.miniprojekti.domain.Video;
 
 public class BookmarkManager {
 
     private Books books;
+    private Videos videos;
+    private Blogs blogs;
     private Podcasts podcasts;
 
-    public BookmarkManager(Books books, Podcasts podcasts) {
+    public BookmarkManager(Books books, Videos videos, Blogs blogs, Podcasts podcasts) {
         this.books = books;
+        this.videos = videos;
+        this.blogs = blogs;
         this.podcasts = podcasts;
     }
 
@@ -21,26 +29,35 @@ public class BookmarkManager {
         this.books.add(author, title, isbn);
     }
 
-    public void addBlog() {
+    public void addVideo(String title, String url, String comment) throws SQLException {
+        this.videos.add(title, url, comment);
+    }
+
+    public void addBlog(String title, String author, String url) throws SQLException {
+        this.blogs.add(title, author, url);
     }
 
     public void addPodcast(String name, String title, String description) throws SQLException {
         this.podcasts.add(name, title, description);
     }
 
-    public void addVideo() {
-    }
-
     public void addTag() {
-        
+
     }
 
     public ArrayList<Book> getBooks() throws SQLException {
         return this.books.getAll();
     }
 
+    public ArrayList<Video> getVideos() throws SQLException {
+        return this.videos.getAll();
+    }
+
+    public ArrayList<Blog> getBlogs() throws SQLException {
+        return this.blogs.getAll();
+    }
+
     public ArrayList<Podcast> getPodcasts() throws SQLException {
         return this.podcasts.getAll();
     }
-
 }
--- src/main/java/ot/miniprojekti/ui/TextUserInterface.java
+++ src/main/java/ot/miniprojekti/ui/TextUserInterface.java
@@ -1,11 +1,12 @@
 package ot.miniprojekti.ui;
 
-import java.util.Scanner;
-import ot.miniprojekti.logic.BookmarkManager;
 import java.sql.SQLException;
-
+import java.util.Scanner;
+import ot.miniprojekti.domain.Blog;
 import ot.miniprojekti.domain.Book;
 import ot.miniprojekti.domain.Podcast;
+import ot.miniprojekti.domain.Video;
+import ot.miniprojekti.logic.BookmarkManager;
 
 public class TextUserInterface {
 
@@ -19,7 +20,7 @@ public class TextUserInterface {
 
     public void start() throws SQLException {
         while (true) {
-            System.out.println("Valitse toiminto");
+            System.out.println("Valitse toiminto:");
             System.out.println("[1] Lisää vinkki");
             System.out.println("[2] Tulosta vinkit");
             System.out.println("[x] Sulje sovellus");
@@ -28,20 +29,11 @@ public class TextUserInterface {
             String answer = reader.nextLine();
 
             if (answer.equals("1")) {
-                System.out.println("[1] Lisää kirja");
-                System.out.println("[2] Lisää Podcast");
-
-                System.out.print("> ");
-                String addType = reader.nextLine();
-
-                if (addType.equals("1")) {
-                    addBookmark();
-                } else if (addType.equals("2")) {
-                    addPodcast();
-                }
-
+                addBookmark();
             } else if (answer.equals("2")) {
                 printBooks();
+                printVideos();
+                printBlogs();
                 printPodcasts();
             } else if (answer.equals("x") || answer.equals("")) {
                 break;
@@ -50,13 +42,54 @@ public class TextUserInterface {
     }
 
     private void addBookmark() throws SQLException {
+        System.out.println("Valitse tyyppi:");
+        System.out.println("[1] Lisää kirja");
+        System.out.println("[2] Lisää video");
+        System.out.println("[3] Lisää blogi");
+        System.out.println("[4] Lisää podcast");
+
+        System.out.print("> ");
+        String answer = reader.nextLine();
+
+        if (answer.equals("1")) {
+            addBook();
+        } else if (answer.equals("2")) {
+            addVideo();
+        } else if (answer.equals("3")) {
+            addBlog();
+        } else if (answer.equals("4")) {
+            addPodcast();
+        }
+    }
+
+    private void addBook() throws SQLException {
         System.out.print("Kirjoittaja: ");
         String author = reader.nextLine();
         System.out.print("Otsikko: ");
         String title = reader.nextLine();
-        System.out.println("ISBN: ");
+        System.out.print("ISBN: ");
         String isbn = reader.nextLine();
         this.bookmarkManager.addBook(author, title, isbn);
+    }
+
+    private void addVideo() throws SQLException {
+        System.out.print("Otsikko: ");
+        String title = reader.nextLine();
+        System.out.print("Url: ");
+        String url = reader.nextLine();
+        System.out.print("Kommentti: ");
+        String comment = reader.nextLine();
+        this.bookmarkManager.addVideo(title, url, comment);
+    }
+
+    private void addBlog() throws SQLException {
+        System.out.print("Otsikko: ");
+        String title = reader.nextLine();
+        System.out.print("Kirjoittaja: ");
+        String author = reader.nextLine();
+        System.out.print("Url: ");
+        String url = reader.nextLine();
+        this.bookmarkManager.addBlog(title, author, url);
     }
 
     private void addPodcast() throws SQLException {
@@ -64,22 +97,34 @@ public class TextUserInterface {
         String name = reader.nextLine();
         System.out.print("Otsikko: ");
         String title = reader.nextLine();
-        System.out.println("Kuvaus: ");
+        System.out.print("Kuvaus: ");
         String description = reader.nextLine();
         this.bookmarkManager.addPodcast(name, title, description);
     }
 
     private void printBooks() throws SQLException {
-        System.out.println("Kirjat");
-        // for (Podcast p : podcasts.getAll()) {
+        System.out.println("Kirjat:");
         for (Book b : bookmarkManager.getBooks()) {
             System.out.println(b.toString());
         }
     }
 
+    private void printVideos() throws SQLException {
+        System.out.println("Videot:");
+        for (Video v : bookmarkManager.getVideos()) {
+            System.out.println(v.toString());
+        }
+    }
+
+    private void printBlogs() throws SQLException {
+        System.out.println("Blogit:");
+        for (Blog b : bookmarkManager.getBlogs()) {
+            System.out.println(b.toString());
+        }
+    }
+
     private void printPodcasts() throws SQLException {
-        System.out.println("Podcastit");
-        // for (Podcast p : podcasts.getAll()) {
+        System.out.println("Podcastit:");
         for (Podcast p : bookmarkManager.getPodcasts()) {
             System.out.println(p.toString());
         }
